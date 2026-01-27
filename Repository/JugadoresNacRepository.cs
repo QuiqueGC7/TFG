f1fcc3287301a226eea836e405fe33b766f6e251
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-01-27 19:50:34 +0100

    JugadoresNacRepository Actualizado

--- Repository/JugadoresNacRepository.cs
+++ Repository/JugadoresNacRepository.cs
@@ -20,7 +20,7 @@ namespace TFG.Repositories
             {
                 await connection.OpenAsync();
 //Falta Modificar las querys (Cuando se acabe la BDD)
-                string query = "SELECT JugadorNacId, Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, PorLibres, DosPts, TresPts FROM JugadoresA";
+                string query = "SELECT JugadorNacId, Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, por2Pts, por3Pts, Valoracion, Rebotes, Asistencias FROM JugadoresNac";
                 using (var command = new SqlCommand(query, connection))
                 {
                     using (var reader = await command.ExecuteReaderAsync())
@@ -29,16 +29,18 @@ namespace TFG.Repositories
                         {
                             var JugadoresNac = new JugadoresNac
                             {
-                                JugadorAId = reader.GetInt32(0),
+                                JugadorNacId = reader.GetInt32(0),
                                 Nombre = reader.GetString(1),
                                 Dorsal = (double)reader.GetDecimal(2),
                                 Posicion = reader.GetBoolean(3),
                                 Equipo = reader.GetString(4),
                                 Puntos = reader.GetDecimal(5),
                                 Libres = reader.GetInt32(6),
-                                PorLibres = reader.GetDecimal(7),
-                                DosPts = reader.GetDecimal(8),
-                                TresPts = reader.GetDecimal(9),
+                                por2Pts = reader.GetDecimal(7),
+                                por3Pts = reader.GetDecimal(8),
+                                Valoracion = reader.GetDecimal(9),
+                                Rebotes = reader.GetDecimal(10),
+                                Asistencias = reader.GetDecimal(11)
                             }; 
 
                             JugadoressNac.Add(JugadoresNac);
@@ -57,7 +59,7 @@ namespace TFG.Repositories
             {
                 await connection.OpenAsync();
 
-                string query = "SELECT Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, PorLibres, DosPts, TresPts FROM JugadoresA WHERE JugadorAId = @Id";
+                string query = "SELECT JugadorNacId, Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, por2Pts, por3Pts, Valoracion, Rebotes, Asistencias FROM JugadoresNac WHERE JugadorNacId = @Id";
                 using (var command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Id", JugadorNacId);
@@ -75,9 +77,11 @@ namespace TFG.Repositories
                                 Equipo = reader.GetString(4),
                                 Puntos = reader.GetDecimal(5),
                                 Libres = reader.GetInt32(6),
-                                PorLibres = reader.GetDecimal(7),
-                                DosPts = reader.GetDecimal(8),
-                                TresPts = reader.GetDecimal(9),
+                                por2Pts = reader.GetDecimal(7),
+                                por3Pts = reader.GetDecimal(8),
+                                Valoracion = reader.GetDecimal(9),
+                                Rebotes = reader.GetDecimal(10),
+                                Asistencias = reader.GetDecimal(11)
                             };
                         }
                     }
@@ -92,7 +96,7 @@ namespace TFG.Repositories
             {
                 await connection.OpenAsync();
 
-                string query = "INSERT INTO JugadoresA (Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, PorLibres, DosPts, TresPts) VALUES (@Nombre, @Dorsal, @Posicion, @Equipo, @Puntos, @Libres, @PorLibres, @DosPts, @TresPts)";
+                string query = "INSERT INTO JugadoresNac (Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, por2Pts, por3Pts, Valoracion, Rebotes, Asistencias) VALUES (@Nombre, @Dorsal, @Posicion, @Equipo, @Puntos, @Libres, @por2Pts, @por3Pts, @Valoracion, @Rebotes, @Asistencias)";
                 using (var command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Nombre", jugadoresNac.Nombre);
@@ -101,9 +105,11 @@ namespace TFG.Repositories
                     command.Parameters.AddWithValue("@Equipo", jugadoresNac.Equipo);
                     command.Parameters.AddWithValue("@Puntos", jugadoresNac.Puntos);
                     command.Parameters.AddWithValue("@Libres", jugadoresNac.Libres);
-                    command.Parameters.AddWithValue("@PorLibres", jugadoresNac.PorLibres);
-                    command.Parameters.AddWithValue("@DosPts", jugadoresNac.DosPts);
-                    command.Parameters.AddWithValue("@TresPts", jugadoresNac.TresPts);
+                    command.Parameters.AddWithValue("@por2Pts", jugadoresNac.por2Pts);
+                    command.Parameters.AddWithValue("@por3Pts", jugadoresNac.por3Pts);
+                    command.Parameters.AddWithValue("@Valoracion", jugadoresNac.Valoracion);
+                    command.Parameters.AddWithValue("@Rebotes", jugadoresNac.Rebotes);
+                    command.Parameters.AddWithValue("@Asistencias", jugadoresNac.Asistencias);
 
                     await command.ExecuteNonQueryAsync();
                 }
@@ -116,19 +122,21 @@ namespace TFG.Repositories
             {
                 await connection.OpenAsync();
 
-                string query = "UPDATE JugadoresA SET Nombre = @Nombre, Dorsal = @Dorsal, Posicion = @Posicion, Equipo = @Equipo, Puntos = @Puntos, Libres = @Libres, PorLibres = @PorLibres, DosPts = @DosPts, TresPts = @TresPts WHERE JugadorAId = @Id";
+                string query = "UPDATE JugadoresNac SET Nombre = @Nombre, Dorsal = @Dorsal, Posicion = @Posicion, Equipo = @Equipo, Puntos = @Puntos, Libres = @Libres, por2Pts = @por2Pts, por3Pts = @por3Pts, Valoracion = @Valoracion, Rebotes = @Rebotes, Asistencias = @Asistencias WHERE JugadorNacId = @Id";
                 using (var command = new SqlCommand(query, connection))
                 {
-                    command.Parameters.AddWithValue("@Id", jugadoresNac.JugadorAId);
                     command.Parameters.AddWithValue("@Nombre", jugadoresNac.Nombre);
                     command.Parameters.AddWithValue("@Dorsal", jugadoresNac.Dorsal);
                     command.Parameters.AddWithValue("@Posicion", jugadoresNac.Posicion);
                     command.Parameters.AddWithValue("@Equipo", jugadoresNac.Equipo);
                     command.Parameters.AddWithValue("@Puntos", jugadoresNac.Puntos);
                     command.Parameters.AddWithValue("@Libres", jugadoresNac.Libres);
-                    command.Parameters.AddWithValue("@PorLibres", jugadoresNac.PorLibres);
-                    command.Parameters.AddWithValue("@DosPts", jugadoresNac.DosPts);
-                    command.Parameters.AddWithValue("@TresPts", jugadoresNac.TresPts);
+                    command.Parameters.AddWithValue("@por2Pts", jugadoresNac.por2Pts);
+                    command.Parameters.AddWithValue("@por3Pts", jugadoresNac.por3Pts);
+                    command.Parameters.AddWithValue("@Valoracion", jugadoresNac.Valoracion);
+                    command.Parameters.AddWithValue("@Rebotes", jugadoresNac.Rebotes);
+                    command.Parameters.AddWithValue("@Asistencias", jugadoresNac.Asistencias);
+
 
                     await command.ExecuteNonQueryAsync();
                 }
@@ -141,7 +149,7 @@ namespace TFG.Repositories
             {
                 await connection.OpenAsync();
 
-                string query = "DELETE FROM JugadoresA WHERE JugadorAId = @Id";
+                string query = "DELETE FROM JugadoresNac WHERE JugadorNacId = @Id";
                 using (var command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Id", JugadorNacId);
@@ -159,34 +167,38 @@ namespace TFG.Repositories
 
                 // Comando SQL para insertar datos iniciales
                 var query = @"
-                    INSERT INTO JugadoresA (Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, PorLibres, DosPts, TresPts)
+                    INSERT INTO JugadoresNac (Nombre, Dorsal, Posicion, Equipo, Puntos, Libres, por2Pts, por3Pts, Valoracion, Rebotes, Asistencias)
                     VALUES 
-                    (@Nombre1, @Dorsal1, @Posicion1, @Equipo1, @Puntos1, @Libres1, @PorLibres1, @DosPts1, @TresPts1),
-                    (@Nombre2, @Dorsal2, @Posicion2, @Equipo2, @Puntos2, @Libres2, @PorLibres2, @DosPts2, @TresPts2)";
+                    (@Nombre1, @Dorsal1, @Posicion1, @Equipo1, @Puntos1, @Libres1, @por2Pts1, @por3Pts1, @Valoracion1, @Rebotes1, @Asistencias1),
+                    (@Nombre2, @Dorsal2, @Posicion2, @Equipo2, @Puntos2, @Libres2, @por2Pts2, @por3Pts2, @Valoracion2, @Rebotes2, @Asistencias2)";
 
                 using (var command = new SqlCommand(query, connection))
                 {
                     // Parámetros para el primer bebida
-                    command.Parameters.AddWithValue("@Nombre1", "Amit");
+                    command.Parameters.AddWithValue("@Nombre1", "Sergio");
                     command.Parameters.AddWithValue("@Dorsal1", 4);
-                    command.Parameters.AddWithValue("@Posicion1", "Ala Pivot");
-                    command.Parameters.AddWithValue("@Equipo1", "2ªAragonesa");
+                    command.Parameters.AddWithValue("@Posicion1", "Base");
+                    command.Parameters.AddWithValue("@Equipo1", "NacionalA2");
                     command.Parameters.AddWithValue("@Puntos1", 4.40);
                     command.Parameters.AddWithValue("@Libres1", 2.1);
-                    command.Parameters.AddWithValue("@PorLibres1", 1.2);
-                    command.Parameters.AddWithValue("@DosPts1", 4.40);
-                    command.Parameters.AddWithValue("@TresPts1", 0.2);
+                    command.Parameters.AddWithValue("@por2Pts1", 1.2);
+                    command.Parameters.AddWithValue("@por3Pts1", 4.40);
+                    command.Parameters.AddWithValue("@Valoracion1", 0.2);
+                    command.Parameters.AddWithValue("@Rebotes1", 0.2);
+                    command.Parameters.AddWithValue("@Asistencias1", 0.2);
 
                     // Parámetros para el segundo bebida
-                    command.Parameters.AddWithValue("@Nombre2", "Mario");
+                    command.Parameters.AddWithValue("@Nombre2", "Varo");
                     command.Parameters.AddWithValue("@Dorsal2", 23);
                     command.Parameters.AddWithValue("@Posicion2", "Escolta");
-                    command.Parameters.AddWithValue("@Equipo2", "3ªAragonesa");
+                    command.Parameters.AddWithValue("@Equipo2", "NacionalA1");
                     command.Parameters.AddWithValue("@Puntos2", 6.33);
                     command.Parameters.AddWithValue("@Libres2", 3.33);
-                    command.Parameters.AddWithValue("@PorLibres2", "Bebida mojada");
-                    command.Parameters.AddWithValue("@DosPts2", 2.40);
-                    command.Parameters.AddWithValue("@TresPts2", 3.33);
+                    command.Parameters.AddWithValue("@por2Pts2", 1.2);
+                    command.Parameters.AddWithValue("@por3Pts2", 4.40);
+                    command.Parameters.AddWithValue("@Valoracion2", 0.2);
+                    command.Parameters.AddWithValue("@Rebotes2", 0.2);
+                    command.Parameters.AddWithValue("@Asistencias2", 0.2);
 
                     await command.ExecuteNonQueryAsync();
                 }
