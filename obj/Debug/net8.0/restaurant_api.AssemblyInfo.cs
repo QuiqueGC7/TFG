2da567331aa6aec5bb5dcde85b85618c7852e937
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-03 17:08:50 +0100

    Rama develop

--- obj/Debug/net8.0/restaurant_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/restaurant_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1fcc3287301a226eea836e405fe33b766f6e251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a710bcc34c25acf1f254e0f16ec17f517d474403")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurant_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurant_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
