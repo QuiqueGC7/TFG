fe65f1b726fad8d5dfaab853f37020dfd6bc334c
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-03 17:19:53 +0100

    ArregloUsers

--- Models/Usuarios.cs
+++
@@ -1,8 +0,0 @@
-public class Usuarios
-{
-    public int UsuarioId { get; set; }
-    public string Nombre { get; set; }
-    public string Apellidos { get; set; }
-    public string DNI { get; set; }
-    public bool Afiliado { get; set; }
-}
