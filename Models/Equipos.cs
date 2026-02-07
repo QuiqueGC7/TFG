92dcaad4a9d98f419a75830100f1dcbd66dc5ed2
QuiqueGC7 <destructor161zgz@gmail.com>
2026-02-07 13:50:11 +0100

    Modelos Equipo y Staff

---
+++ Models/Equipos.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TFG.Models
+{
+    public class Equipos
+    {
+        public int IdEquipo { get; set; }
+        public string Nombre { get; set; }
+        public string Victorias { get; set; }
+        public string Derrotas { get; set; }
+    }
+}
---
+++ Models/Staff.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TFG.Models
+{
+    public class Staff
+    {
+        public int IdJugador { get; set; }
+        public string Nombre { get; set; }
+        public string Puesto { get; set; }
+        public string Posicion { get; set; }
+    }
+}
