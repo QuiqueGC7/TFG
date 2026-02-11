210a5d36aac3e69277a2772a91ed4b35c8a43561
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-11 19:56:33 +0100

    --

--- obj/Debug/net8.0/TFG.AssemblyInfo.cs
+++ obj/Debug/net8.0/TFG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75ddea33873f8e929d19e1127c20c4ee469929b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69d2e7036da6e8702f3c15e135e4f5181c73b997")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
