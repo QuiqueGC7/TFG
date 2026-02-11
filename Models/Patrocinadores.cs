c5623d5cec825b82c2afbe39b6a8fc56a9d93d69
Taazz7 <garciacbzc@gmail.com>
2026-02-11 19:09:28 +0100

    vaciado de restaurante de rama main

--- Models/Patrocinadores.cs
+++
@@ -1,7 +0,0 @@
-public class Patrocinadores
-{
-    public int PatrocinadorId { get; set; }
-    public string Nombre { get; set; }
-    public int CantidadAportada { get; set; }
-    public int Duración { get; set; }
-}
--- Models/Roles.cs
+++
@@ -1,9 +0,0 @@
-namespace Models;
-
-public static class Roles
-{
-    public const string Admin = "admin";
-    public const string User = "user";
-    public const string Guest = "guest";
-}
-
